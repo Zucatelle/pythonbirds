25abb29be4c20d1e5aad514537738a0497d0475e
Zucatelle <jbzucatelle@gmail.com>
2019-08-06 09:05:50 -0300

    Criado e removido atributo dinamico do tipo pessoa

--- oo/carro.py
+++ oo/carro.py
@@ -17,74 +17,94 @@ A direção controlara a direção. Ela oferecera os seguintes atributos:
     O   L
       S
       Exemplo
-motor = Motor()
-motor.velocidade
+>>> motor = Motor()
+>>> motor.velocidade
 0
-motor.acelerar()
-motor.velocidade
+>>> motor.acelerar()
+>>> motor.velocidade
 1
-motor.acelerar()
-motor.velociade
+>>> motor.acelerar()
+>>> motor.velocidade
 2
-motor.acelerar(0
-motor.velocidade
+>>> motor.acelerar()
+>>> motor.velocidade
 3
-motor.frear()
-motor.velocidade
+>>> motor.frear()
+>>> motor.velocidade
 1
-motor.frear()
-motor.velocidade
+>>> motor.frear()
+>>> motor.velocidade
 0
-# Teste direção
-direcao - Direcao()
-direcao.valor
+
+ Teste direção
+
+>>> direcao = Direcao()
+>>> direcao.valor
 'Norte"
-direcao_girar_a_direita()
-direcao.valor
+>>> direcao_girar_a_direita()
+>>> direcao.valor
 'Leste"
-direcao_girar_a_direita()
-direcao.valor
+>>> direcao_girar_a_direita()
+>>> direcao.valor
 'Sul"
-direcao_girar_a_direita()
-direcao.valor
+>>> direcao_girar_a_direita()
+>>> direcao.valor
 'Oeste"
-direcao_girar_a_direita()
-direcao.valor
+>>> direcao_girar_a_direita()
+>>> direcao.valor
 'Norte"
-direcao_girar_a_esquerda()
-direcao.valor
+>>> direcao_girar_a_esquerda()
+>>> direcao.valor
 'Oeste"
-direcao_girar_a_esquerda()
-direcao.valor
+>>> direcao_girar_a_esquerda()
+>>> direcao.valor
 'Sul"
-direcao_girar_a_esquerda()
-direcao.valor
+>>> direcao_girar_a_esquerda()
+>>> direcao.valor
 'Leste"
-direcao_girar_a_esquerda()
-direcao.valor
+>>> direcao_girar_a_esquerda()
+>>> direcao.valor
 'Norte"
-carro = Carro(direcao,motor)
-carro.calcular_velocidade()
+>>> carro = Carro(direcao,motor)
+>>> carro.calcular_velocidade()
 0
-carro.acelerar()
-carro.calcular_velocidade()
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
 1
-carro.acelerar()
-carro.calcular_velocidade()
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
 2
-carro.frear()
-carro.calcular_velocidade()
+>>> carro.frear()
+>>> carro.calcular_velocidade()
 0
-carro.calcular_direcao()
+>>> carro.calcular_direcao()
 "Norte"
-carro.girar_a_direita()
-carro.calcular direcao()
+>>> carro.girar_a_direita()
+>>> carro.calcular direcao()
 'Leste'
-carro.girar_a_esquerda()
-carro.calcular direcao()
+>>> carro.girar_a_esquerda()
+>>> carro.calcular direcao()
 'Norte'
-carro.girar_a_esquerda()
-carro.calcular direcao()
+>>> carro.girar_a_esquerda()
+>>> carro.calcular direcao()
 'Oeste'
 
 """
+
+class Motor():
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade = max(0, self.velocidade - 2)
+        #self.velocidade -= 2
+        #if self.velocidade < 0:
+        #    self.velocidade = 0
+
+
+
+
+
