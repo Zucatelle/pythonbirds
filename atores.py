e955459356793affb354076ce4c0e9d883e4cc75
Zucatelle <jbzucatelle@gmail.com>
2019-08-27 16:19:02 -0300

    27-08-19

--- atores.py
+++ atores.py
@@ -121,7 +121,17 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return self.x, self.y
+        if self.foi_lancado():
+            delta_t = tempo - self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+        return super().calcular_posicao(tempo)
+
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual = self._y_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        y_atual -= (GRAVIDADE * (delta_t **2)) / 2
+        self.y = y_atual
 
 
     def lancar(self, angulo, tempo_de_lancamento):
@@ -139,7 +149,9 @@ class Passaro(Ator):
 
 
 class PassaroAmarelo(Passaro):
-    pass
+    velocidade_escalar = 30
+    _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
 
     #_caracter_ativo = 'A'
 
